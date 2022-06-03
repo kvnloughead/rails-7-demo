c8acbea947aa2e178f010510550ad2e73b411689
kvnloughead <kvnloughead@gmail.com>
2022-06-02 21:24:03 -0400

    refactor: add full_title helper for page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# Contains application level helpers
+# Put controller level controllers in controller_helper.rb
 module ApplicationHelper
+
+  # Returns full title on a per-page basis. String interpolation is avoided 
+  # because it can produce double escaping. 
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    page_title.empty? ? base_title : page_title + ' | ' + base_title  
+  end
+
 end
