443b200aa3b4d2e279fbc04cdb673822cd545cd0
kvnloughead <kvnloughead@gmail.com>
2022-06-11 10:21:31 -0400

    Add email index and case insensitive uniqueness constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  # Downcase email before saving user to ensure case insensitive uniqueness
+  # before_save { self.email = self.email.downcase }
+  before_save { self.email.downcase! }
 
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, 
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 end
---
+++ db/migrate/20220611140617_add_index_to_user_email.rb
@@ -0,0 +1,6 @@
+# Adds index to User.email to prevent full page scans
+class AddIndexToUserEmail < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :email, unique: true
+  end
+end
