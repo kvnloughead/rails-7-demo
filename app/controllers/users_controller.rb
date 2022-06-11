f91936eb284aa85abd9141c105db15e1f4ccc7fa
kvnloughead <kvnloughead@gmail.com>
2022-06-11 17:20:11 -0400

    Add users/:id SHOW route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
+    debugger
+
+  end
+
+  def show
+    # injects @user variable with appropriate id into the view
+    # url: root_path/users?id=user-id
+    @user = User.find(params[:id])
   end
 end
