48a0f97e490e10fe2a9e903952f1fc644c3bed94
kvnloughead <kvnloughead@gmail.com>
2022-06-14 20:17:09 -0400

    Implement error messages for signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,22 @@ class UsersController < ApplicationController
     # url: root_path/users?id=user-id
     @user = User.find(params[:id])
   end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      # success
+    else
+      # HTTP status 422. This is necessary in Rails 7 
+      # when rendering regular HTML with Turbo
+      render 'new', status: :unprocessable_entity 
+    end
+  end
+
+  private
+    # Create secure hash of parameters for User model
+    def user_params
+      params.require(:user).permit(:name, :email, :password,            
+                                   :password_confirmation)
+    end
 end
