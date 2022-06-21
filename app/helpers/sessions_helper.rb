c22a1330086530ad3f90066577c0a9209c99c6d7
kvnloughead <kvnloughead@gmail.com>
2022-06-20 20:22:51 -0400

    Add current_user helper for use in views

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,14 @@ module SessionsHelper
     # session method is a Rails built-in. 
     # Sessions created in this way end when the browser window is closed.
     session[:user_id] = user.id
+  end
 
+  # Returns the current logged-in user (if any)
+  def current_user
+    if session[:user_id]
+      # find_by returns nil if no matching document found
+      # note that (a ||= b) is equivalent to (a = a || b)
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
