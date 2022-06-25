9f779dfa20956aa52676e6da63a3dba58af1bb0e
kvnloughead <kvnloughead@gmail.com>
2022-06-25 11:36:57 -0400

    Update header and footer styles on mobile

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,11 +1,19 @@
 // Menu manipulation
 
-// Add toggle listeners to listen for clicks.
+// Add toggle listeners to hamburger and account toggle
 document.addEventListener('turbo:load', function () {
-  let account = document.querySelector('#account');
+  const hamburger = document.querySelector('#hamburger');
+  const navbarMenu = document.querySelector('#navbar-menu');
+
+  hamburger.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    navbarMenu.classList.toggle('collapse');
+  });
+
+  const account = document.querySelector('#account');
   account.addEventListener('click', function (event) {
     event.preventDefault();
-    let menu = document.querySelector('#dropdown-menu');
+    const menu = document.querySelector('#dropdown-menu');
     menu.classList.toggle('active');
   });
 });
