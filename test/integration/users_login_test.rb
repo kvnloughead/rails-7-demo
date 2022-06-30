638d2aaa3a6c4a8f546abdf79e7392592e96ba26
kvnloughead <kvnloughead@gmail.com>
2022-06-29 21:53:13 -0400

    Refactor user_login tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,67 +1,79 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
-    @invalid_user = {
-      email: "bad@email.com",
-      password: "asdfasdf"
-    }
-
     @user = users(:test_user)
+  end 
+
+  class InvalidPasswordTest < UsersLogin
+    
+    test "login path" do
+      get login_path
+      assert_template 'sessions/new'
+    end
+
+    test "login with valid email/invalid password" do
+      post login_path, params: { session: { email: @user.email, password: "" } }
+      assert_response :unprocessable_entity 
+      assert_template 'sessions/new'
+      assert_not flash.empty?
+      
+      # flash is gone after rendering a different endpoint
+      get root_path
+      assert flash.empty?
+    end
+
+    test "login with completely invalid information" do
+      post login_path, params: { session: { email: "", password: "" } }
+      assert_response :unprocessable_entity 
+      assert_template 'sessions/new'
+      assert_not flash.empty?
+      
+      # flash is gone after rendering a different endpoint
+      get root_path
+      assert flash.empty?
+    end
   end
 
-  test "login with invalid information" do
-    get login_path
-    assert_template 'sessions/new'
-
-    post login_path, params: { session: { email: "", password: "" } }
-    assert_response :unprocessable_entity 
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    
-    # flash is gone after rendering a different endpoint
-    get root_path
-    assert flash.empty?
+  class ValidLogin < UsersLogin
+    def setup
+      super
+      post login_path, params: { session: { email: @user.email, password: "password" } }
+    end
   end
 
-  test "login with valid email / invalid password" do
-    get login_path
-    assert_template 'sessions/new'
+  class ValidLoginTest < ValidLogin
+    test "valid login" do
+      assert_equal @user.id, session[:user_id]
+      assert_redirected_to @user
+    end
 
-    post login_path, params: { session: { email: @user.email, password: "invalid" } }
-    
-    assert_not_equal @user.id, session[:user_id]
-    assert_response :unprocessable_entity 
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    
-    # flash is gone after rendering a different endpoint
-    get root_path
-    assert flash.empty?
-  end
+    test "redirect after login" do
+      follow_redirect!
+      assert_template 'users/show'
+      assert_select "a[href=?]", login_path, count: 0
+      assert_select "a[href=?]", logout_path
+      assert_select "a[href=?]", user_path(@user)
+    end
 
-  test "login with valid information followed by logout" do
+    class Logout < ValidLogin
+      def setup
+        super
+        delete logout_path
+      end
 
-    post login_path, params: { session: { email: @user.email, 
-                                          password: "password" } }
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+      test "successful logout" do
+        assert_not_equal @user.id, session[:user_id]
+        assert_response :see_other
+        assert_redirected_to root_url
+      end
 
-    # Checks that user's id is the id that's stored in the session
-    # Alternative to the is_logged_in? handler from Learn Enough 
-    # https://www.learnenough.com/ruby-on-rails-7th-edition-tutorial/basic_login#code-test_helper_session
-    assert_equal @user.id, session[:user_id]
-
-    delete logout_path
-    assert_not_equal @user.id, session[:user_id]
-    assert_response :see_other
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+      test "redirect after logout" do
+        follow_redirect!
+        assert_select "a[href=?]", login_path
+        assert_select "a[href=?]", logout_path,      count: 0
+        assert_select "a[href=?]", user_path(@user), count: 0
+      end
+    end
   end
 end
