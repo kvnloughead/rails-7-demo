8634f11203573d0cf80baed46ae5be5cc177613e
kvnloughead <kvnloughead@gmail.com>
2022-07-14 20:24:47 -0400

    Add user seed data, using faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# The jdata can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+
+# After updating, run 
+#   $ rails db:migrate:reset
+#   $ rails db:seed
+
+# Create primary sample user.
+# create! throws an exception when failing instead of simply returning false
+User.create!(name: "Example User", 
+             email: "example@railstutorial.org",
+             password: "asdfasdf",
+             password_confirmation: "asdfasdf")
+
+# Generate additional random users.
+99.times do |n|
+  User.create!(name:                  Faker::Name.name,
+               email:                 "example-#{n+1}@railstutorial.org", 
+               password:              "password",
+               password_confirmation: "password")
+end
