708192133ef37a157e34592647b52c6c97a3eb09
kvnloughead <kvnloughead@gmail.com>
2022-07-16 21:09:14 -0400

    Generate AccountActivation resource and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get "/help", to: "static_pages#help"        # named path: help_path
-  get "/about", to: "static_pages#about"      # named path: about_path
-  get "/contact", to: "static_pages#contact"  # named path: contact_path
+  get "/help", to: "static_pages#help"        # help_path
+  get "/about", to: "static_pages#about"      # about_path
+  get "/contact", to: "static_pages#contact"  # contact_path
 
-  get "/signup", to: "users#new"              # named path: signup_path
-  get "/login", to: "sessions#new"            # named path: login_path
+  get "/signup", to: "users#new"              # signup_path
+  get "/login", to: "sessions#new"            # login_path
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   
-  # Provides full suite of REST routes.
+  # `resources` build-in provides full suite of REST routes.
   # See https://guides.rubyonrails.org/routing.html for details.
   resources :users
+  resources :account_activations, only: [:edit] # only need edit action
 end
